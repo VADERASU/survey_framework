75f3842fe6b7fb6e9b0c4bbbda052da8ae5ab713
Rostyslav Hnatyshyn <rostyslav.hnatyshyn@gmail.com>
2023-06-22 15:57:03 -0700

    improved modal layout

--- src/api/front/src/components/PaperModal.jsx
+++ src/api/front/src/components/PaperModal.jsx
@@ -1,4 +1,5 @@
 import Dialog from '@mui/material/Dialog';
+import DialogContent from '@mui/material/DialogContent';
 import Chip from '@mui/material/Chip';
 import Stack from '@mui/material/Stack';
 import Box from '@mui/material/Box';
@@ -18,17 +19,21 @@ export default function PaperModal({
     const theme = useTheme();
 
     return (
-        <Dialog open={open} onClose={handleClose} scroll="body">
-            <Box sx={{ display: 'flex', margin: 3, gap: '10px' }}>
-                <img width={200} height={200} style={{ border: '1px solid' }} src={`data:image/png;base64,${thumbnail}`} />
-                <Stack spacing={2}>
-                    <Typography><b>{title}</b></Typography>
-                    <Typography>{author}</Typography>
-                    <Box sx={{ display: 'flex', gap: '3px' }}>
+        <Dialog fullWidth maxWidth='sm' open={open} onClose={handleClose} scroll="body">
+            <DialogContent>
+                <Stack gap={1}>
+                    <Stack direction="row" gap={1}>
+                        <img width={200} height={200} style={{ border: '1px solid' }} src={`data:image/png;base64,${thumbnail}`} />
+                        <Stack spacing={2}>
+                            <Typography><b>{title}</b></Typography>
+                            <Typography>{author}</Typography>
+                        </Stack>
+                    </Stack>
+                    <Stack direction="row" gap={1}>
                         {keywords.map((k) => <Chip sx={{ backgroundColor: theme.palette[k].main }} key={k} variant="outlined" label={k} />)}
-                    </Box>
+                    </Stack>
                 </Stack>
-            </Box>
-        </Dialog>
+            </DialogContent>
+        </Dialog >
     );
 }
